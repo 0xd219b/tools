e0885bf48aa405edf8d596b29aa3d3fc2467677c
bojun.cbj <bojun.cbj@gmail.com>
2021-07-02 22:17:40 +0800

    feat: init project and add raycast: switch screen
    Signed-off-by: bojun.cbj <bojun.cbj@gmail.com>

---
+++ raycast/readme.md
@@ -0,0 +1,3 @@
+# Raycast command script or workflow
+
+[用raycast快捷切换屏幕焦点](switch-screen)
---
+++ raycast/switch-screen/j2r.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title j2r
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description jump to right screen
+# @raycast.author bojun.cbj
+
+/usr/local/bin/cliclick c:[x,y]
